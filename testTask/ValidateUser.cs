1d214287dc6fb634054f6c0f577c51696f87355c
Каріна Белань <belankarina3@gmail.com>
2022-03-21 13:32:49 +0200

    Add project files.

---
+++ testTask/ValidateUser.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace testTask
+{
+    public class ValidateUser : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    var phone = entity.GetAttributeValue<string>("kb_telephone");
+
+                    Regex regex = new Regex(@"^[0-9]{10}$");
+
+                    if (!regex.IsMatch(phone))
+                    {
+                        throw new InvalidPluginExecutionException("False.");
+                    }
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("catch An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+
+}
---
+++ testTask/isValidated_script.js
@@ -0,0 +1,9 @@
+﻿function isValidated(executionContext)
+{
+    var formContext = executionContext.getFormContext();
+    var phone = formContext.getAttribute("kb_telephone").getValue();
+    const RE = new RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/);
+    alert (RE.test(phone))
+
+}
+//123-345-3456-true
